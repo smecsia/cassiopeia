7e675ecce0dc05c0009364b658f5d1610a985b52
Ilya <smecsia@waveaccess.ru>
2010-03-04 13:40:55 +0300

    Server service_url fix

--- lib/cassiopeia/server.rb
+++ lib/cassiopeia/server.rb
@@ -4,19 +4,23 @@ module Cassiopeia
     @instance = nil
     def cas_data(session)
       {
-        Cassiopeia::CONFIG[:ticket_id_key] => session[Cassiopeia::CONFIG[:ticket_id_key]]
+        TICKET_KEY => session[TICKET_KEY]
       }
     end
     public 
     def self.instance
       return @instance if @instance
       @instance = Cassiopeia::Server.new
+      SERVICE_KEY = Cassiopeia::CONFIG[:service_url_key]
+      TICKET_KEY = Cassiopeia::CONFIG[:ticket_id_key]
     end
 
     def service_url(session)
-      session[Cassiopeia::CONFIG[:service_url_key]] + "?" + hash_to_query(cas_data(session))
+      if session && session[SERVICE_KEY] && session[TICKET_KEY]
+        session[SERVICE_KEY] + "?" + hash_to_query(cas_data(session))
+      end
     end
 
   end
-  
+
 end
